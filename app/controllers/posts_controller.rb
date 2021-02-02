a807e9a432a0011447355eb1dfd3f4fb439f0c82
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2021-02-02 19:25:18 +0200

    DRY code and add partial layout to posts folder in views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
+    before_action :set_post, except: [:index, :new, :create]
 
     def show
-        @post = Post.find(params[:id])
     end 
 
     def index
@@ -23,11 +23,9 @@ class PostsController < ApplicationController
     end 
 
     def edit
-        @post = Post.find(params[:id])
     end 
 
     def update 
-        @post = Post.find(params[:id])
         if @post.update(white_list)
             flash[:notice] = "Post was updated successfully."
             redirect_to @post
@@ -37,14 +35,18 @@ class PostsController < ApplicationController
     end 
 
     def destroy 
-        @post = Post.find(params[:id])
         @post.destroy
         redirect_to posts_path
     end
 
     private
+
     def white_list
         params.require(:post).permit(:title, :category, :fulfilled, :body)
     end
+
+    def set_post
+        @post = Post.find(params[:id])
+    end 
 
 end
