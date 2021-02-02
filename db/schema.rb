9a515fa7f36ed672ba37f7393a027357ad897bc3
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2021-02-02 19:42:19 +0200

    Migrate database to Mysql because Herouku doesn't work with Sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_02_01_174122) do
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "body"
