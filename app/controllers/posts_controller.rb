e1f49a322a3de28bc16d708d4277d7d28df7b5e2
MennaTullahTaha <menna.tullah.magdy731@gmail.com>
2021-02-02 16:06:16 +0200

    Add create Posts feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
         @posts = Post.all
     end 
 
+    def new
+    end 
+
+    def create
+        @post = Post.new(white_list)
+        @post.save
+        redirect_to @post
+    end 
+
+    private
+    def white_list
+        params.require(:post).permit(:title, :category, :fulfilled, :body)
+    end
+
 end
